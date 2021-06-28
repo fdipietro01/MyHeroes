9e7334d6533edc832e77b75ac5353556609f13e0
Flavio <fdipietro01@gmail.com>
2021-06-28 16:13:04 -0300

    App extrathings deleted

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext } from "react";
 import {LogIn} from "./container/LogIn/LogIn"
 import {Home} from "./Home/Home"
 import {Seeker} from "./container/Seeker/Seeker"
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom"
-import {LogInAuth} from "./contexts/LogInContext/LogInContext"
 import {TeamSelection} from "./contexts/TeamContext/TeamContext"
 import {HeroeDetail} from "../src/container/HeroeDetail/HeroeDetail"
 import {PrivateRoute} from "./container/RoutHandler/PrivateRoute";
